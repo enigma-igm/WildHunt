a1a9390b0230bbd303d101b63cc8e7c239eac0ea
Rik-ad-astra <riccardo.nanni@hotmail.it>
2022-02-18 15:22:58 +0100

    Working on WSA VSA

--- wildhunt/surveys/vsa_wsa.py
+++ wildhunt/surveys/vsa_wsa.py
@@ -1,9 +1,6 @@
-#!/usr/bin/env python
-
 import time
 import os
 import numpy as np
-from astropy.io import fits
 import multiprocessing
 from multiprocessing import Process, Queue
 from IPython import embed
@@ -15,13 +12,247 @@ os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
 os.environ["OPENBLAS_NUM_THREADS"] = "1"
 os.environ["MKL_NUM_THREADS"] = "1"
 
-# Survey parameters
-survey_param = {'archive':'WSA','database':'UKIDSSDR11PLUS','programmeID':'101','bands':('Y','H','K'),'idPresent':'noID',
-                'userX':'0.5', 'email':'','email1':'','crossHair':'n','mode':'wget'}
-
+# List of programmeID: VHS - 110, VVV - 120, VMC - 130, VIKING - 140, VIDEO - 150, UHS - 107, UKIDSS: LAS - 101,
+# GPS - 102, GCS - 103, DXS - 104, UDS - 105
 numCoords=10000  # number of coords in each bach (do not exceed 20000)
 
-boundary="--FILEUPLOAD"  # separator
+class VsaWsa(object):
+
+    def __init__(self, bands, fov, name, survey):
+        """
+        :param bands:
+        :param fov:
+        :param name:
+        """
+        self.name = name
+        self.bands = bands
+        self.fov = fov
+        self.survey = survey
+        if self.survey[0] == 'U':
+            archive='WSA'
+            if self.survey[0:3] == 'UKI':
+                if self.survey[-3::] == 'LAS': programID='101'
+                if self.survey[-3::] == 'GPS': programID = '102'
+                if self.survey[-3::] == 'GCS': programID = '103'
+                if self.survey[-3::] == 'DXS': programID = '104'
+                if self.survey[-3::] == 'UDS': programID = '105'
+                self.survey=self.survey[:-3]
+            if self.survey[0:3] == 'UHS': programID = '107'
+        else:
+            archive='VSA'
+            if self.survey[0:3]=='VHS': programID='110'
+            if self.survey[0:3] == 'VVV': programID = '120'
+            if self.survey[0:3] == 'VMC': programID = '130'
+            if self.survey[0:3] == 'VIK': programID = '140'
+            if self.survey[0:3] == 'VID': programID = '150'
+
+        self.archive=archive
+        self.programID=programID
+
+        super(VsaWsa, self).__init__(bands, fov, survey)
+
+    def download_images(self, ra, dec, image_folder_path, n_jobs):
+        """
+        :param ra:
+        :param dec:
+        :param image_folder_path:
+        :param n_jobs:
+        :return:
+        """
+
+        self.download_links(ra,dec)
+
+        raise NotImplementedError
+
+    def download_links(self, ra, dec):
+        '''
+        Download the links to the images and produce all the *_wget.sh files
+        :param ra:
+        :param dec:
+        :param image_folder_path:
+        :param n_jobs:
+        :return:
+        '''
+
+        # Survey parameters
+        survey_param = {'archive': self.archive, 'database': self.survey, 'programmeID': self.programID, 'bands': self.bands,
+                        'idPresent': 'noID', 'userX': '0.5', 'email': '', 'email1': '', 'crossHair': 'n',
+                        'mode': 'wget'}
+        boundary = "--FILEUPLOAD"  # separator
+
+        if np.size(ra) > numCoords:
+            nbatch = int(np.ceil(np.size(ra) / numCoords))
+        else:
+            nbatch = 1
+
+        loop = 0
+        for i in range(nbatch):
+            startID = numCoords * loop
+            loop += 1
+            uploadFile = "upload_" + str(loop) + ".txt"
+            outFile = "out_" + str(loop) + ".txt"
+            up = open(uploadFile, 'w+')
+
+            for j in survey_param:
+                if j == 'bands':
+                    for band in (survey_param[j]):
+                        print(boundary, file=up)
+                        print('Content-Disposition: form-data; name=\"band\"\n', file=up)
+                        print(band + ' ', file=up)
+                else:
+                    print(boundary, file=up)
+                    print('Content-Disposition: form-data; name=\"{}\"\n'.format(j), file=up)
+                    print(str(survey_param[j]) + ' ', file=up)
+            print(boundary, file=up)
+            print('Content-Disposition: form-data; name=\"startID\"\n', file=up)
+            print(str(startID) + ' ', file=up)
+            print(boundary, file=up)
+            print(
+                'Content-Disposition: form-data; name=\"fileName\"; filename=\"{}\" Content-Type: text/plain\n'.format(
+                    uploadFile), file=up)
+
+            if i != (nbatch - 1):
+                n = numCoords
+            else:
+                n = len(ra) - startID
+            for j in range(n):
+                if dec[startID + j] >= 0:
+                    print('{:>15},{:>15}'.format('+' + str(ra[startID + j]), '+' + str(dec[startID + j])), file=up)
+                else:
+                    print('{:>15},{:>15}'.format('+' + str(ra[startID + j]), str(dec[startID + j])), file=up)
+            print('\n', file=up)
+            print(boundary + '--', file=up)
+            up.close()
+
+            if archive=='WSA':
+                os.system(
+                    "wget --keep-session-cookies --header=\"Content-Type: multipart/form-data;  boundary=FILEUPLOAD\""
+                      " --post-file {} http://wsa.roe.ac.uk:8080/wsa/tmpMultiGetImage -O {}".format(uploadFile,
+                                                                                                    outFile))
+            else:
+                os.system(
+                    "wget --keep-session-cookies --header=\"Content-Type: multipart/form-data;  boundary=FILEUPLOAD\""
+                    " --post-file {} http://horus.roe.ac.uk:8080/vdfs/MultiGetImage -O {}".format(uploadFile,
+                                                                                                  outFile))
+
+            out = open(outFile, 'r')
+            Lines = out.readlines()
+
+            dlFile = str(loop) + '_wget.sh'
+            sh = open(dlFile, 'w')
+
+            j = 0
+            n_bands = 1
+            for line in Lines:
+                record = '--http'
+                if (record in line):
+                    name, namebrief = hms2name(ra[startID + j], dec[startID + j])  # Name of the images to download
+                    if n_bands == len(survey_param['bands']):
+                        j += 1
+                        n_bands = 0
+                    n_bands += 1
+                    strtPos = line.index(record)
+                    endPos = line.rindex('-->')
+                    fileURL = line[strtPos + 2:endPos]
+                    if ('band=NULL' in line):
+                        print('Skip download')
+                    else:
+                        band_url = line.index('band=') + len('band=')
+                        name_url = name + '.' + line[band_url] + '.fits.gz'
+                        sh.write("wget \"{}\" -O {}\n".format(fileURL, name_url))
+            out.close()
+            sh.close()
+            print("Start sleep")
+            time.sleep(5)
+            print("End sleep")
+
+    def download_parallel(self, ra, n_process=1):
+        '''
+            Download many sources in parallel
+            Args:
+                coadd_id:
+                ra_id:
+                dec_id:
+                n_process
+        '''
+        if np.size(ra) > numCoords:
+            nbatch = int(np.ceil(np.size(ra) / numCoords))
+        else:
+            nbatch = 1
+        n_file = nbatch
+        n_cpu = multiprocessing.cpu_count()
+
+        if n_process > n_cpu:
+            n_process = n_cpu
+
+        if n_process > n_file:
+            n_process = n_file
+
+        work_queue = Queue()
+        processes = []
+
+        for ii in range(n_file):
+            work_queue.put([ii])
+
+        for w in range(n_process):
+            p = Process(target=self.download_images, args=(work_queue,))
+            processes.append(p)
+            p.start()
+
+        for p in processes:
+            p.join()
+
+    def download_images(self, work_queue):
+        '''
+        Execute the *_wget.sh files to download the images
+        '''
+        while not work_queue.empty():
+            nbatch = work_queue.get()[0]
+            os.system("bash {}_wget.sh".format(str(nbatch + 1)))
+            sh = open('{}_finished.txt'.format(str(nbatch + 1)), 'w')
+            sh.close()
+
+    def gzip_parallel(self, ra, dec, n_process=1):
+        '''
+            Unzip images for several sources in parallel
+            Args:
+                coadd_id:
+                ra_id:
+                dec_id:
+                n_process
+        '''
+        n_file = np.size(ra)
+        n_cpu = multiprocessing.cpu_count()
+
+        if n_process > n_cpu:
+            n_process = n_cpu
+
+        if n_process > n_file:
+            n_process = n_file
+
+        work_queue = Queue()
+        processes = []
+
+        for ii in range(n_file):
+            work_queue.put([ra[ii], dec[ii]])
+
+        for w in range(n_process):
+            p = Process(target=unzip_images, args=(work_queue,))
+            processes.append(p)
+            p.start()
+
+        for p in processes:
+            p.join()
+
+    def unzip_images(self, work_queue):
+        while not work_queue.empty():
+            ra, dec = work_queue.get()
+            name, namebrief = hms2name(ra, dec)
+            print("Unzipping {} source".format(name))
+            os.system("gzip -d -f {}.Y.fits.gz".format(name))
+            os.system("gzip -d -f {}.H.fits.gz".format(name))
+            os.system("gzip -d -f {}.K.fits.gz".format(name))
+
 
 def deg2hms(ra, dec):
     rah = np.floor(ra / 15.0)
@@ -65,185 +296,4 @@ def hms2name(ra, dec):
     name = 'J' + rass + decss
     namebrief = name[0:5] + name[11:-6]
     return name, namebrief
-
-def download_links(ra,dec):
-    '''
-    Download the links to the images and produce all the *_wget.sh files
-    '''
-    if np.size(ra)>numCoords:
-        nbatch=int(np.ceil(np.size(ra)/numCoords))
-    else:
-        nbatch=1
-
-    loop = 0
-    for i in range(nbatch):
-        startID=numCoords * loop
-        loop+=1
-        uploadFile="upload_"+str(loop)+".txt"
-        outFile="out_"+str(loop)+".txt"
-        up = open(uploadFile, 'w+')
-
-        for j in survey_param:
-            if j=='bands':
-                for band in (survey_param[j]):
-                    print(boundary, file=up)
-                    print('Content-Disposition: form-data; name=\"band\"\n', file=up)
-                    print(band+' ', file=up)
-            else:
-                print(boundary,file=up)
-                print('Content-Disposition: form-data; name=\"{}\"\n'.format(j),file=up)
-                print(str(survey_param[j])+' ',file=up)
-        print(boundary, file=up)
-        print('Content-Disposition: form-data; name=\"startID\"\n', file=up)
-        print(str(startID)+' ', file=up)
-        print(boundary, file=up)
-        print('Content-Disposition: form-data; name=\"fileName\"; filename=\"{}\" Content-Type: text/plain\n'.format(uploadFile), file=up)
-
-        if i!=(nbatch-1):
-            n=numCoords
-        else:
-            n=len(ra)-startID
-        for j in range(n):
-            if dec[startID+j]>=0:
-                print('{:>15},{:>15}'.format('+'+str(ra[startID + j]), '+'+str(dec[startID + j])), file=up)
-            else:
-                print('{:>15},{:>15}'.format('+'+str(ra[startID+j]), str(dec[startID+j])), file=up)
-        print('\n',file=up)
-        print(boundary+'--', file=up)
-        up.close()
-
-        os.system("wget --keep-session-cookies --header=\"Content-Type: multipart/form-data;  boundary=FILEUPLOAD\""
-                  " --post-file {} http://wsa.roe.ac.uk:8080/wsa/tmpMultiGetImage -O {}".format(uploadFile,outFile))
-
-        out = open(outFile,'r')
-        Lines = out.readlines()
-
-        dlFile = str(loop) + '_wget.sh'
-        sh = open(dlFile, 'w')
-
-        j=0
-        n_bands=1
-        for line in Lines:
-            record='--http'
-            if (record in line):
-                name, namebrief=hms2name(ra[startID + j],dec[startID + j]) # Name of the images to download
-                if n_bands==len(survey_param['bands']):
-                    j+=1
-                    n_bands=0
-                n_bands+=1
-                strtPos=line.index(record)
-                endPos=line.rindex('-->')
-                fileURL=line[strtPos+2:endPos]
-                if ('band=NULL' in line):
-                    print('Skip download')
-                else:
-                    band_url=line.index('band=')+len('band=')
-                    name_url=name+'.'+line[band_url]+'.fits.gz'
-                    sh.write("wget \"{}\" -O {}\n".format(fileURL,name_url))
-        out.close()
-        sh.close()
-        print("Start sleep")
-        time.sleep(5)
-        print("End sleep")
-
-def download_parallel(ra,n_process=16):
-    '''
-        Perform forced photometry for several sources
-        Args:
-            coadd_id:
-            ra_id:
-            dec_id:
-            n_process
-    '''
-    if np.size(ra)>numCoords:
-        nbatch=int(np.ceil(np.size(ra)/numCoords))
-    else:
-        nbatch=1
-    n_file = nbatch
-    n_cpu = multiprocessing.cpu_count()
-
-    if n_process>n_cpu:
-        n_process = n_cpu
-
-    if n_process>n_file:
-        n_process = n_file
-
-    work_queue = Queue()
-    processes = []
-
-    for ii in range(n_file):
-        work_queue.put([ii])
-
-    for w in range(n_process):
-        p = Process(target=download_images, args=(work_queue,))
-        processes.append(p)
-        p.start()
-
-    for p in processes:
-        p.join()
-
-def download_images(work_queue):
-    '''
-    Execute the *_wget.sh files to download the images
-    '''
-    while not work_queue.empty():
-        nbatch = work_queue.get()[0]
-        os.system("bash {}_wget.sh".format(str(nbatch+1)))
-        sh=open('{}_finished.txt'.format(str(nbatch+1)),'w')
-        sh.close()
-
-def gzip_parallel(ra,dec,n_process=16):
-    '''
-        Unzip images for several sources in parallel
-        Args:
-            coadd_id:
-            ra_id:
-            dec_id:
-            n_process
-    '''
-    n_file = np.size(ra)
-    n_cpu = multiprocessing.cpu_count()
-
-    if n_process>n_cpu:
-        n_process = n_cpu
-
-    if n_process>n_file:
-        n_process = n_file
-
-    work_queue = Queue()
-    processes = []
-
-    for ii in range(n_file):
-        work_queue.put([ra[ii],dec[ii]])
-
-    for w in range(n_process):
-        p = Process(target=unzip_images, args=(work_queue,))
-        processes.append(p)
-        p.start()
-
-    for p in processes:
-        p.join()
-
-def unzip_images(work_queue):
-    while not work_queue.empty():
-        ra, dec = work_queue.get()
-        name, namebrief = hms2name(ra, dec)
-        print("Unzipping {} source".format(name))
-        os.system("gzip -d -f {}.Y.fits.gz".format(name))
-        os.system("gzip -d -f {}.H.fits.gz".format(name))
-        os.system("gzip -d -f {}.K.fits.gz".format(name))
-
-# Main code to launch the software
-start_time = time.time()
-par = fits.open('total_noDELSDR9_2arcsec_Jselected_UKIDSS_DELS_filtered.fits')
-#par = fits.open('UKIDSS_subsample.fits')
-data = par[1].data
-ra, dec = data['ra'], data['dec']
-nbatch=download_links(ra,dec)
-download_parallel(ra,n_process=5)
-gzip_parallel(ra,dec,n_process=25)
-
-print("Elapsed time: {0:.2f} sec" .format(time.time() - start_time))
-f = open('time_spent.txt','w')
-print(time.time()-start_time,file=f)
 
