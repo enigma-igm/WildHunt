aadce67f0bba55384174f50e5807f52be4d34087
Rik-ad-astra <riccardo.nanni@hotmail.it>
2022-02-24 14:21:34 +0100

    Adding LS image download

--- examples/download_example.py
+++ examples/download_example.py
@@ -9,8 +9,8 @@ df = pd.read_csv('UKIDSS_sources.csv')
 
 survey_dict = [
                {'survey': 'ps1', 'bands': ['g', 'r', 'i', 'z', 'y'], 'fov':50},
-               {'survey': 'UKIDSSDR11PLUSLAS', 'bands': ['Y', 'J', 'H', 'K'],
-                'fov':50}
+               {'survey': 'UKIDSSDR11PLUSLAS', 'bands': ['Y', 'J', 'H', 'K'],'fov':50},
+               {'survey': 'DELSDR9', 'bands': ['g', 'r', 'z', '1', '2'],'fov':50}
                 ]
 
 image.get_images(df['RA'].values[:20], df['DEC'].values[:20], 'cutouts',
---
+++ wildhunt/surveys/legacysurvey.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+import numpy as np
+from astropy.table import Table
+
+from wildhunt.surveys import imagingsurvey
+
+
+from IPython import embed
+
+
+class LegacySurvey(imagingsurvey.ImagingSurvey):
+
+    def __init__(self, bands, fov, name, verbosity=1):
+        """
+
+        :param bands:
+        :param fov:
+        :param name:
+        """
+
+        super(LegacySurvey, self).__init__(bands, fov, name, verbosity)
+
+    def download_images(self, ra, dec, image_folder_path, n_jobs=1):
+        """
+
+        :param ra:
+        :param dec:
+        :param image_folder_path:
+        :param n_jobs:
+        :return:
+        """
+
+        self.survey_setup(ra, dec, image_folder_path, epoch='J', n_jobs=n_jobs)
+
+        if len(ra) == len(dec) and len(ra) > 0:
+
+            self.retrieve_image_url_list()
+
+            self.check_for_existing_images_before_download()
+
+
+
+            if self.n_jobs > 1:
+                self.mp_download_image_from_url()
+            else:
+                for idx in self.download_table.index:
+                    image_name = self.download_table.loc[idx, 'image_name']
+                    url = self.download_table.loc[idx, 'url']
+                    self.download_image_from_url(url, image_name)
+
+        else:
+
+            print('All images already exist.')
+            print('Downloading aborted.')
+
+    def retrieve_image_url_list(self):
+
+        for idx in self.source_table.index:
+
+            ra = self.source_table.loc[idx, 'ra']
+            dec = self.source_table.loc[idx, 'dec']
+            obj_name = self.source_table.loc[idx, 'obj_name']
+            release = 'ls-dr' + self.name[6:]
+            bands = ''.join(self.bands)
+
+            for band in bands:
+
+                # Convert field of view in arcsecond to pixel size (1 pixel = 0.262 arcseconds
+                # for g,r,z and 2.75 arcseconds for W1, W2)
+                if band in ['1','2']:
+                    pixelscale = 2.75
+                else:
+                    pixelscale = 0.262
+                size = self.fov * 1 / pixelscale
+
+                # Create image name
+                image_name = obj_name + "_" + self.name + "_" + \
+                             band + "_fov" + '{:d}'.format(self.fov)
+
+                url = ("http://legacysurvey.org/viewer/fits-cutout/?ra={}&dec={}&layer={}"
+                       "&pixscale={}&bands={}&size={}").format(ra,dec,release,str(pixelscale),band,str(int(size)))
+
+
+                self.download_table = self.download_table.append(
+                    {'image_name': image_name,'url': url},
+                    ignore_index=True)
