f7df96f4adea4ffd0fc64f3e5ff9e3b57497f120
Jan-Torge Schindler <schindler@mpia.de>
2022-02-24 21:24:24 +0100

    updated ps1 image download with bulk retrieval of urls. Unclear what the maximum number of sources is. Maybe cap at 1000 or 10000. This should now be faster than the preivous PS1 image download.

--- examples/download_example.py
+++ examples/download_example.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 
+import time
 import pandas as pd
 
 from wildhunt import image
 
+t0 = time.time()
 
 df = pd.read_csv('UKIDSS_sources.csv')
 
+
+
 survey_dict = [
                {'survey': 'PS1', 'bands': ['g', 'r', 'i', 'z', 'y'], 'fov':50},
-               {'survey': 'UKIDSSDR11PLUSLAS', 'bands': ['Y', 'J', 'H', 'K'],
-                'fov':50},
-               {'survey': 'DELSDR9', 'bands': ['g', 'r', 'z', '1', '2'],
-                'fov':50}
+               # {'survey': 'UKIDSSDR11PLUSLAS', 'bands': ['Y', 'J', 'H', 'K'],
+               #  'fov':50},
+               # {'survey': 'DELSDR9', 'bands': ['g', 'r', 'z', '1', '2'],
+               #  'fov':50}
                 ]
 
 image.get_images(df['RA'].values[:20], df['DEC'].values[:20], 'cutouts',
                  survey_dict,
                  n_jobs=400)
+print("{:.1f} s: ".format( time.time() - t0))
--- wildhunt/surveys/panstarrs.py
+++ wildhunt/surveys/panstarrs.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
+
+import requests
 import numpy as np
+from io import StringIO
 from astropy.table import Table
 
 from wildhunt.surveys import imagingsurvey
@@ -35,11 +38,9 @@ class Panstarrs(imagingsurvey.ImagingSurvey):
 
         if len(ra) == len(dec) and len(ra) > 0:
 
-            self.retrieve_image_url_list()
+            self.retrieve_image_url_list(imagetypes="stack")
 
             self.check_for_existing_images_before_download()
-
-
 
             if self.n_jobs > 1:
                 self.mp_download_image_from_url()
@@ -54,64 +55,125 @@ class Panstarrs(imagingsurvey.ImagingSurvey):
             print('All images already exist.')
             print('Downloading aborted.')
 
-    def retrieve_image_url_list(self):
+    def retrieve_image_url_list(self, imagetypes="stack"):
 
         # Convert field of view in arcsecond to pixel size (1 pixel = 0.25 arcseconds)
         size = self.fov * 4
 
-        for idx in self.source_table.index:
+        ra = self.source_table.loc[:, 'ra'].values
+        dec = self.source_table.loc[:, 'dec'].values
 
-            ra = self.source_table.loc[idx, 'ra']
-            dec = self.source_table.loc[idx, 'dec']
-            obj_name = self.source_table.loc[idx, 'obj_name']
-
-            bands = ''.join(self.bands)
-
-            try:
-                filetable = self.get_ps1_filetable(ra, dec, bands=bands)
-
-            except :
-                fname = '{}_download_table_temp.csv'.format(self.name)
-                self.download_table.to_csv(fname)
-                raise Exception('Download of url table failed. Wrote survey '
-                                'download table to file: {}'.format(fname))
-
-            for row in filetable:
-                band = row['filter']
-                filename = row['filename']
-
-                # Create image name
-                image_name = obj_name + "_" + self.name + "_" + \
-                             band + "_fov" + '{:d}'.format(self.fov)
-
-                url = ("https://ps1images.stsci.edu/cgi-bin/fitscut.cgi?"
-                       "ra={}&dec={}&size={}&format=fits").format(ra,
-                                                                  dec,
-                                                                  size)
-
-                urlbase = url + "&red="
-
-                self.download_table = self.download_table.append(
-                    {'image_name': image_name,
-                     'url': urlbase + filename},
-                    ignore_index=True)
+        bands = ''.join(self.bands)
 
 
-    def get_ps1_filetable(self, ra, dec, bands='g'):
-        """
+        # Retrieve bulk file table
+        url_ps1filename = 'http://ps1images.stsci.edu/cgi-bin/ps1filenames.py?'
 
-        :param ra:
-        :param dec:
-        :param bands:
-        :return:
-        """
-        url_base = 'http://ps1images.stsci.edu/cgi-bin/ps1filenames.py?'
-        ps1_url = url_base + 'ra={}&dec={}&filters={}'.format(ra, dec, bands)
+        # Put the positions in an in-memory file object
+        cbuf = StringIO()
+        cbuf.write(
+            '\n'.join(
+                ["{} {}".format(ra_idx, dec_idx) for (ra_idx, dec_idx) in zip(
+                    ra, dec)]))
+        cbuf.seek(0)
 
-        table = Table.read(ps1_url, format='ascii')
+        # Use requests.post to pass in positions as a file
+        r = requests.post(url_ps1filename,
+                          data=dict(filters=bands, type=imagetypes),
+                          files=dict(file=cbuf))
+        r.raise_for_status()
+        # Convert retrieved file table to pandas DataFrame
+        df = Table.read(r.text, format="ascii").to_pandas()
 
-        # Sort filters from red to blue
-        flist = ["yzirg".find(x) for x in table['filter']]
-        table = table[np.argsort(flist)]
+        # Group table by ra and do not sort!
+        groupby = df.groupby(by='ra', sort=False)
 
-        return table
+        for idx, (key, group) in enumerate(groupby):
+
+            obj_name = self.source_table.iloc[idx]['obj_name']
+            ra_idx = self.source_table.iloc[idx]['ra']
+            dec_idx = self.source_table.iloc[idx]['dec']
+
+            if key == ra_idx:
+
+                for jdx in group.index:
+                    band = group.loc[jdx, 'filter']
+                    filename = group.loc[jdx, 'filename']
+
+                    # Create image name
+                    image_name = obj_name + "_" + self.name + "_" + \
+                                 band + "_fov" + '{:d}'.format(self.fov)
+
+                    url = ("https://ps1images.stsci.edu/cgi-bin/fitscut.cgi?"
+                           "ra={}&dec={}&size={}&format=fits").format(ra_idx,
+                                                                      dec_idx,
+                                                                      size)
+                    urlbase = url + "&red="
+
+                    self.download_table = self.download_table.append(
+                        {'image_name': image_name,
+                         'url': urlbase + filename},
+                        ignore_index=True)
+
+
+    # def retrieve_image_url_list(self):
+    #
+    #     # Convert field of view in arcsecond to pixel size (1 pixel = 0.25 arcseconds)
+    #     size = self.fov * 4
+    #
+    #     for idx in self.source_table.index:
+    #
+    #         ra = self.source_table.loc[idx, 'ra']
+    #         dec = self.source_table.loc[idx, 'dec']
+    #         obj_name = self.source_table.loc[idx, 'obj_name']
+    #
+    #         bands = ''.join(self.bands)
+    #
+    #         try:
+    #             filetable = self.get_ps1_filetable(ra, dec, bands=bands)
+    #
+    #         except :
+    #             fname = '{}_download_table_temp.csv'.format(self.name)
+    #             self.download_table.to_csv(fname)
+    #             raise Exception('Download of url table failed. Wrote survey '
+    #                             'download table to file: {}'.format(fname))
+    #
+    #         for row in filetable:
+    #             band = row['filter']
+    #             filename = row['filename']
+    #
+    #             # Create image name
+    #             image_name = obj_name + "_" + self.name + "_" + \
+    #                          band + "_fov" + '{:d}'.format(self.fov)
+    #
+    #             url = ("https://ps1images.stsci.edu/cgi-bin/fitscut.cgi?"
+    #                    "ra={}&dec={}&size={}&format=fits").format(ra,
+    #                                                               dec,
+    #                                                               size)
+    #
+    #             urlbase = url + "&red="
+    #
+    #             self.download_table = self.download_table.append(
+    #                 {'image_name': image_name,
+    #                  'url': urlbase + filename},
+    #                 ignore_index=True)
+    #
+    #
+    # def get_ps1_filetable(self, ra, dec, bands='g'):
+    #     """
+    #
+    #     :param ra:
+    #     :param dec:
+    #     :param bands:
+    #     :return:
+    #     """
+    #     url_base = 'http://ps1images.stsci.edu/cgi-bin/ps1filenames.py?'
+    #     ps1_url = url_base + 'ra={}&dec={}&filters={}'.format(ra, dec, bands)
+    #
+    #     table = Table.read(ps1_url, format='ascii')
+    #
+    #     # Sort filters from red to blue
+    #     flist = ["yzirg".find(x) for x in table['filter']]
+    #     table = table[np.argsort(flist)]
+    #
+    #     return table
