1ec40a301807ffc7893a8e1846a2180f993825d0
Rik-ad-astra <riccardo.nanni@hotmail.it>
2022-04-07 13:40:58 +0200

    Corrected some typos for the download with the VSA server

--- wildhunt/catalog.py
+++ wildhunt/catalog.py
@@ -46,7 +46,7 @@ astroquery_dict = {
                               'ra': 'RAJ2000', 'dec': 'DECJ2000',
                               'mag_name': 'R', 'mag': 'Rmag', 'distance':
                               'distance', 'data_release': None},
-                    'vhsdr6': {'service': 'vsa', 'catalog': 'VHS',
+                    'vhsdr6': {'service': 'VSA', 'catalog': 'VHS',
                                'ra': 'ra', 'dec': 'dec',
                                'data_release': 'VHSDR6', 'mag_name': 'VHS_J',
                                'mag': 'jAperMag3', 'distance': 'distance'},
@@ -56,7 +56,7 @@ astroquery_dict = {
                                        'UKIDSS_J',
                                'mag': 'jAperMag3', 'distance': 'distance'},
                     # new, needs to be tested!
-                    'vikingdr5': {'service': 'vsa', 'catalog': 'VIKING',
+                    'vikingdr5': {'service': 'VSA', 'catalog': 'VIKING',
                                'ra': 'ra', 'dec': 'dec',
                                'data_release': 'VIKINGDR5', 'mag_name': 'VHS_J',
                                'mag': 'jAperMag3', 'distance': 'distance'}
