ec9f70480f9fcbaaf5a92ebc470c237c2c7958d7
jtschindler <jtschindler@email.arizona.edu>
2022-08-26 10:32:21 +0200

    updated example

--- examples/catalog_merge_example.py
+++ examples/catalog_merge_example.py
@@ -6,12 +6,14 @@ from wildhunt import catalog
 
 from IPython import embed
 
+print("Hallo")
+
 if __name__ == '__main__':
 
 
     dtype = dtype={'comment': 'object',
        'obs_date': 'object'}
-    candidates = catalog_new.Catalog('candidates',
+    candidates = catalog.Catalog('candidates',
                                      datapath='data/reclassified_all_observations_upto1901_clean.csv',
                                      ra_column_name='obs_sdss_ra',
                                      dec_column_name='obs_sdss_dec',
@@ -27,7 +29,7 @@ if __name__ == '__main__':
              'simbad_ref': 'object',
              'simbad_type': 'object'}
 
-    obs = catalog_new.Catalog('observed',
+    obs = catalog.Catalog('observed',
                               datapath='data/new_elqs_quasars.csv',
                               ra_column_name='wise_ra_x',
                               dec_column_name='wise_dec_x',
