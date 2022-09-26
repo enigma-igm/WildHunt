47e5b0b7b200938677415b830adbeac80f03c0ea
Rik-ad-astra <riccardo.nanni@hotmail.it>
2022-09-26 13:19:13 +0200

    Updating observing preparation example

--- examples/observation_preparation_example.py
+++ examples/observation_preparation_example.py
@@ -2,80 +2,76 @@
 
 import pandas as pd
 
-from wildhunt import catalog_queries as whcat
-from wildhunt import findingchart as whfc
-from wildhunt import image as whim
+from wildhunt import catalog as whcat
+from wildhunt import findingchart
 
-# Load candidate list
+if __name__ == '__main__':
+    df = pd.read_csv('LRIS_VI_candidates.csv')
 
-candidates = pd.read_csv('LRIS_VI_candidates.csv')
+    # Load example catalog
+    cat = whcat.Catalog('qso_candidates',
+                        ra_column_name='RA',
+                        dec_column_name='DEC',
+                        id_column_name='Name',
+                        table_data=df)
 
+    # Create offset star catalog with datalab
+    datalab_dict = {'table': 'ls_dr9.tractor',
+                    'ra': 'ra',
+                    'dec': 'dec',
+                    'mag': 'mag_r',
+                    'mag_name': 'lsdr9_r'}
 
-# Query for offset stars using
+    quality_query = "mag_r > 15 AND mag_r < 20.5 AND type = 'PSF'"
 
-# Define datalab dictionary
-datalab_dict = {'survey': 'ls_dr9',
-                'table': 'tractor',
-                'ra': 'ra',
-                'dec': 'dec',
-                'mag': 'mag_r',
-                'mag_name': 'lsdr9_r'}
+    cat.get_offset_stars_datalab(100, datalab_dict=datalab_dict,
+                                 where=quality_query)
 
-# Radius should be adjusted to FOV of acquisition camera
-radius = 50
+    df = pd.read_csv('{}_{}_OFFSETS.csv'.format(
+        'qso_candidates', datalab_dict['table'].split('.')[0]))
 
-# The magnitude filter band and range should be adjusted regarding the filter
-# used for acquisition. Also, it is wise to only select point sources as
-# offset stars.
-where = "mag_r > 1 AND mag_r < 20 and type='PSF'"
+    '''
+    # Create offset star catalog with PS1
 
-# Get the offset star table
-offsets = whcat.get_offset_stars_datalab(candidates, 'Name', 'RA', 'DEC',
-                                         radius,
-                                         datalab_dict, where=where,
-                                         verbosity=2)
+    quality_query = "yMeanPSFMag > 15 and yMeanPSFMag < 19"
 
-offsets.to_csv('offset_stars.csv')
+    cat.get_offset_stars_ps1(100, quality_query=quality_query)
 
-# Download the cutouts
-survey_dict = [
-               {'survey': 'DELSDR9', 'bands': ['r'],
-               'fov':110}
-                ]
+    df = pd.read_csv('qso_candidates_ps1_OFFSETS.csv')
+    '''
+    # Download the cutouts
+    survey_dict = [
+        {'survey': 'DELSDR9', 'bands': ['r'],
+         'fov': 150}
+    ]
 
-whim.get_images(candidates['RA'].values,
-                 candidates['DEC'].values,
-                 'cutouts',
-                 survey_dict,
-                 n_jobs=8)
+    cat.get_survey_images('cutouts', survey_dict, n_jobs=10)
 
+    # Create the finding charts
+    ra_column_name = 'target_ra'
+    dec_column_name = 'target_dec'
+    target_column_name = 'target_name'
+    survey = 'DELSDR9'
+    band = 'r'
+    aperture = 2
+    fov = 150
+    image_folder_path = './cutouts'
 
-# Create the finding charts
-ra_column_name = 'RA'
-dec_column_name = 'DEC'
-target_column_name = 'Name'
-survey = 'DELSDR9'
-band = 'r'
-aperture = 2
-fov = 110
-image_folder_path = './cutouts'
+    offsets = pd.read_csv('qso_candidates_ls_dr9_OFFSETS.csv')
 
-offsets = pd.read_csv('offset_stars.csv')
-
-whfc.make_finding_charts(candidates, ra_column_name, dec_column_name,
-                         target_column_name, survey, band,
-                         aperture, fov, image_folder_path,
-                         offset_table=offsets,
-                         offset_id=0,
-                         offset_focus=False,
-                         offset_ra_column_name='offset_ra',
-                         offset_dec_column_name='offset_dec',
-                         pos_angle_column_name='pos_angle',
-                         offset_mag_column_name='mag_r',
-                         offset_id_column_name='offset_shortname',
-                         format='png',
-                         # slit_width=5,
-                         # slit_length=100,
-                         auto_download=False)
-
+    findingchart.make_finding_charts(df, ra_column_name, dec_column_name,
+                                     target_column_name, survey, band,
+                                     aperture, fov, image_folder_path,
+                                     offset_table=offsets,
+                                     offset_id=0,
+                                     offset_focus=False,
+                                     offset_ra_column_name='offset_ra',
+                                     offset_dec_column_name='offset_dec',
+                                     pos_angle_column_name='pos_angle',
+                                     offset_mag_column_name='mag_r',
+                                     offset_id_column_name='offset_shortname',
+                                     format='png',
+                                     # slit_width=5,
+                                     # slit_length=100,
+                                     auto_download=False)
 
